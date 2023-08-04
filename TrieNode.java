9a760bd935cc972e6edceb2be7f074feb26268a0
unknown <kyare@unsa.edu.pe>
2023-08-04 09:06:54 -0500

    Nodo del arbol Trie

---
+++ TrieNode.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+public class TrieNode {
+    private Map<Character, TrieNode> children;
+    private boolean isEndOfWord;
+
+    public TrieNode() {
+        children = new HashMap<>();
+        isEndOfWord = false;
+    }
+
+    public Map<Character, TrieNode> getChildren() {
+        return children;
+    }
+
+    public boolean isEndOfWord() {
+        return isEndOfWord;
+    }
+
+    public void setEndOfWord(boolean endOfWord) {
+        isEndOfWord = endOfWord;
+    }
+}
