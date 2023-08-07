0e92d106abb0ec45c0c3c438ee33cb8f24b5a0e3
Anette-Gallegos <agallegosco@unsa.edu.pe>
2023-08-07 02:21:37 -0500

    TrieNode: Agregando comentarios

--- TrieNode.java
+++ TrieNode.java
@@ -1,23 +1,24 @@
 import java.util.HashMap;
 import java.util.Map;
+
 public class TrieNode {
-    private Map<Character, TrieNode> children;
-    private boolean isEndOfWord;
+    private Map<Character, TrieNode> children; // Mapa que mapea caracteres a nodos hijos
+    private boolean isEndOfWord; // Indica si este nodo representa el final de una palabra
 
     public TrieNode() {
-        children = new HashMap<>();
-        isEndOfWord = false;
+        children = new HashMap<>(); // Inicializa el mapa de hijos
+        isEndOfWord = false; // Inicializa isEndOfWord como falso por defecto
     }
 
     public Map<Character, TrieNode> getChildren() {
-        return children;
+        return children; // Obtiene el mapa de hijos del nodo actual
     }
 
     public boolean isEndOfWord() {
-        return isEndOfWord;
+        return isEndOfWord; // Verifica si este nodo representa el final de una palabra
     }
 
     public void setEndOfWord(boolean endOfWord) {
-        isEndOfWord = endOfWord;
+        isEndOfWord = endOfWord; // Establece si este nodo representa el final de una palabra
     }
 }
