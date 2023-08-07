81b63d3dd8999ca9117ece0fff6f705877c306d5
Anette-Gallegos <agallegosco@unsa.edu.pe>
2023-08-07 09:47:07 -0500

    PlagiarismCheker: Actualizando métodos

--- PlagiarismCheker.java
+++ PlagiarismCheker.java
@@ -1,29 +1,58 @@
 import java.util.Random;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+
 public class PlagiarismCheker {
     private Trie databaseTrie;
     private double detectionErrorRate = 0.2;
 
     /*
-     * @param path: Rutas de los archivos que forman la BD.
-     * 
-     * @return: Booleans informando que no hubo errores con la lectura
-     * 
+     * Carga los archivos de la base de datos en la estructura Trie.
+     *
+     * @param paths: Rutas de los archivos que forman la base de datos.
+     * @return: true si la carga se realizó correctamente.
      */
-    public boolean loadFiles(String[] path) {
+    public boolean loadFiles(String[] paths) {
         databaseTrie = new Trie();
-        // Llenar las estructuras (recomendado)
-        // Lectura del archivo (recomendado)
-        for (String filePath : path) {
-            databaseTrie.insert(filePath);
+        for (String filePath : paths) {
+            String content = readFile(filePath);
+            if (!content.isEmpty()) {
+                databaseTrie.insert(content);
+            }
         }
         return true;
     }
 
     /*
-     * @param path: Rutas del archivo donde colocaremos el texto con/sin plagio
-     * 
-     * @return: Resultados del sistema de deteccion de plagio
+     * Lee el contenido de un archivo y lo devuelve como un String.
+     *
+     * @param filePath: Ruta del archivo a leer.
+     * @return: Contenido del archivo como un String.
+     */
+    private String readFile(String filePath) {
+        try {
+            StringBuilder content = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+            reader.close();
+            return content.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ""; // Si hay un error al leer el archivo, devuelve una cadena vacía
+        }
+    }
+
+    /*
+     * Verifica si hay plagio en el texto proporcionado comparándolo con la base de datos.
+     *
+     * @param path: Ruta del archivo que contiene el texto a verificar.
+     * @return: Resultados del sistema de detección de plagio.
      */
     public ResultChecker verifyPlagiarism(String path) {
         if (databaseTrie == null) {
@@ -37,6 +66,7 @@ public class PlagiarismCheker {
             boolean isPlagiarized = databaseTrie.search(path);
             double randomValue = random.nextDouble();
 
+            // Evaluar si el texto es plagio o no, teniendo en cuenta el error de detección
             if (isPlagiarized && randomValue > detectionErrorRate) {
                 result.setResult(i, true);
             } else if (!isPlagiarized && randomValue <= detectionErrorRate) {
